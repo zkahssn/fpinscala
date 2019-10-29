7ca7d56bd428d8e33cc38d35d47b66cf28967dc4
Zakia Hossain <zakiahossain01@gmail.com>
2019-10-29 23:30:23 +0000

    variance function

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -59,7 +59,10 @@ object Option extends App {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+
+//  math.pow(x - m, 2)
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap(m => mean(xs.map(x => math.pow(x-m, 2))))
 
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
