907cd9e221a10fc4b854b77aacb64419348d720f
Zakia Hossain <=>
2019-11-18 17:44:30 +0000

    lazy functions

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,59 +3,72 @@ package fpinscala.laziness
 import scala.collection.immutable.Stream._
 
 trait Stream[+A] {
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
-    this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
-      case _ => z
-    }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
-    case Empty => None
+    case Empty      => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
 
-
   def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n-1))
+    case Cons(h, t) if n > 1  => Stream.cons(h(), t().take(n - 1))
     case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
-    case _ => Stream.empty
+    case _                    => Stream.empty
   }
 
   def drop(n: Int): Stream[A] = this match {
-    case Cons(_ , t) if n > 0 => t().drop(n-1)
-    case _ => this
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _                   => this
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = this match{
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
     case Cons(h, t) if p(h()) => t().takeWhile(p)
     case _                    => this
   }
 
   def takeWhileFold(p: A => Boolean): Stream[A] = {
-    this.foldRight(Stream.empty[A])((a, b) => if(p(a)){Stream.cons(a,b)} else{Stream.empty}
+    this.foldRight(Stream.empty[A])(
+      (a, b) => if (p(a)) { Stream.cons(a, b) } else { Stream.empty }
     )
   }
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 
-  def headOptionFold: Option[A] = this.foldRight(None: Option[A])((a,b) => Some(a))
+  def headOptionFold: Option[A] =
+    this.foldRight(None: Option[A])((a, b) => Some(a))
 
-  def headOption: Option[A] = this match{
-    case Empty => None
+  def headOption: Option[A] = this match {
+    case Empty      => None
     case Cons(h, t) => Some(h())
   }
+  // The arrow `=>` in front of the argument type `B` means that the function
+  // `f` takes its second argument by name and may choose not to evaluate it.
+  // If `f` doesn't evaluate its second argument, the recursion never occurs.
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) =>
+        f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+  def map[B](m: A => B): Stream[B] =
+    this.foldRight(Stream.empty[B])((a, b) => Stream.cons(m(a), b))
+
+  def filter(f: A => Boolean): Stream[A] =
+    this.foldRight(Stream.empty[A])(
+      (h, t) => if (f(h)) Stream.cons(h, t) else t
+    )
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 
-  def toList: List[A] = this match{
-    case Empty => Nil
-    case Cons(h, t) => h()::t().toList
+  def toList: List[A] = this match {
+    case Empty      => Nil
+    case Cons(h, t) => h() :: t().toList
   }
 
 }
@@ -73,15 +86,14 @@ object Stream extends App {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
   def from(n: Int): Stream[Int] = ???
 
-
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
 
-  val myStream: Stream[Int] = Stream(1,2,3,0,5,6,7)
-  println("PRINT =>" + myStream.headOptionFold)
+  val myStream: Stream[Int] = Stream(1, 2, 3, 2, 0, 16, 7, 8, 9)
+  println("mapping " + myStream.filter((x: Int) => x > 3).toList)
 }
